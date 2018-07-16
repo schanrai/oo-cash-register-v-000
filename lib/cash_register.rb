2b488b720523f8c4ae4ba417820a6e4e8ff4fde7
Sushi Chanrai <sushi@pathworks.co>
2018-07-16 19:12:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,12 +8,13 @@
 require'pry'
 class CashRegister
 
-  attr_accessor :total,:discount,:last_transaction
+  attr_accessor :total,:discount, :items, :last_transaction
 #why don't you need amount and title defined as well as the attributes above? there is no behaviour ofr funcitonality to price or title they are sttic d[ieces of data ]
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    items = []
   end
 
   def add_item (title, amount, quantity =1)

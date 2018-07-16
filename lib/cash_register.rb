8227f99a917c20dbe7002c706fd93934ac3076f2
Sushi Chanrai <sushi@pathworks.co>
2018-07-16 19:11:08 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,7 +26,7 @@ class CashRegister
       return "There is no discount to apply."
     else
       @total = @total * ((100.0 - discount) / 100.0)
-      return "After the discount, the total comes to $#{@total}.to_i."
+      return "After the discount, the total comes to $#{@total.to_i}."
     #format("%1.f",a)
     #binding.pry
     end
